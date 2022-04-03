255f10517787848e8ac698a9a0fb3e2fbce6073b
Smashdogz <markhurleyps3@gmail.com>
2022-04-03 20:30:20 +0100

    reworked project

---
+++ Assets/Explorer.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using SmartDLL;
+
+public class Explorer : MonoBehaviour
+{
+    public Text eText;
+    public Button openExplorerButton;
+
+    public SmartFileExplorer fileExplorer = new SmartFileExplorer();
+
+    private bool showText;
+
+    private bool readText = false;
+
+    void OnEnable()
+    {
+        openExplorerButton.onClick.AddListener(delegate { ShowExplorer(); });
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fileExplorer.resultOK && showText)
+        ReadText(fileExplorer.fileName);
+        readText = false;
+    }
+
+  void ShowExplorer()
+    {
+        string initialDir = @"C:\";
+        bool restoreDir = true;
+        string title = "Open a Text File";
+        string defExt = "txt";
+        string filter = "txt files (*.txt)|*.txt";
+
+        fileExplorer.OpenExplorer(initialDir,restoreDir,title,defExt,filter);
+        readText = true;
+    }
+
+    void ReadText(string path)
+    {
+        eText.text = File.ReadAllText(path);
+    }
+}
